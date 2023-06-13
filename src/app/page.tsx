6cbb941e717ad70722fcdaf1c2b2b9b5d853bcfb
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2023-06-12 22:19:04 -0300

    c

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import variables from './variables.module.scss'
-
-export default function Home() {
-  return (
-    <main>
-      <div style={{ color: variables.primaryColor }}>Hello world</div>
-    </main>
-  )
-}
