7bec9a5f5a8410fd1569982942edafa800ac379e
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2023-06-12 22:39:45 -0300

    c

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import { Inter } from 'next/font/google'
+import { useState } from 'react'
+import style from '../styles/Home.module.scss'
 
 export default function Home() {
+  const [isDark, setIsDark] = useState<boolean>(false)
+
   return (
     <>
-      <main>
+      <main className={`${style.main} ${isDark ? style.dark : ''}`} onClick={()=>{setIsDark(!isDark)}}>
         hello world
       </main>
     </>
